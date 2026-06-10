280b2dccad5d687ce9864165892a18ec1358d271
crestongetz <167683310+crestongetz@users.noreply.github.com>
2026-06-10 08:18:12 -0400

    Module 6 completed baseline

---
+++ app_admin/src/app/models/trip.ts
@@ -0,0 +1,11 @@
+export interface Trip {
+    _id: String,
+    code: String,
+    name: String,
+    length: String, 
+    start: Date,
+    resport: String, 
+    perPerson: String,
+    image: String,
+    description: String
+}
